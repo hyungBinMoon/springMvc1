31d076f1d014a360f86439a5235420858f275992
mhb <gudqls97@naver.com>
2022-08-21 13:21:47 +0900

    [220821] 뷰 리졸버

--- src/main/java/hello/springMvc/web/springmvc/old/OldController.java
+++ src/main/java/hello/springMvc/web/springmvc/old/OldController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.Controller;
 public class OldController implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("OldController.handleRequest");
+		return new ModelAndView("new-form");
 	}
 }
